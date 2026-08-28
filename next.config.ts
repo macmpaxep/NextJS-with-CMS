2dbbd751fee82a91c066cbfede4b8f8b813e8c87
Magzhan <mz@MacBook-Air--Magzhan.local>
2026-08-28 17:05:56 +0500

    ignore ts errors in build

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,13 @@
-import type { NextConfig } from "next";
 import { withPayload } from "@payloadcms/next/withPayload";
+import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  typescript: {
+    ignoreBuildErrors: true,
+  },
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
 };
 
 export default withPayload(nextConfig);
